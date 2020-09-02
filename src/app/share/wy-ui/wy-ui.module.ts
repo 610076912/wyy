f0ab8aaba64cafe65d6b059c38f9e29e35514ab6
yaolp <102342984@qq.com>
2020-09-02 18:08:47 +0800

    20200902

--- src/app/share/wy-ui/wy-ui.module.ts
+++ src/app/share/wy-ui/wy-ui.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { SingleSheetComponent } from './single-sheet/single-sheet.component';
 import { PlayCountPipe } from '../play-count.pipe';
+import { WyPlayerModule } from './wy-player/wy-player.module';
 
 @NgModule({
   declarations: [
     SingleSheetComponent,
     PlayCountPipe
   ],
-  imports: [],
+  imports: [WyPlayerModule],
   exports: [
     SingleSheetComponent,
-    PlayCountPipe
+    PlayCountPipe,
+    WyPlayerModule
   ]
 })
 export class WyUiModule {
